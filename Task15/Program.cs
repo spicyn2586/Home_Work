5f3bc1e8a5f80667d83d7d2854f2d5fd824d6cf2
SpitcynAV <spicyn.2000@mail.ru>
2023-06-30 20:05:54 +0300

    Решил задание 15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.WriteLine("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+if (1 <= number & number <= 7)
+{
+    if (number == 6)
+    {
+        Console.Write($"{number} -> Да");
+    }
+    else if (number == 7)
+    {
+        Console.Write($"{number} -> Да");
+    }
+    else
+    {
+        Console.Write($"{number} -> Нет");
+    }
+}
+else
+{
+     Console.Write($"{number} -> Это не день недели");
+}
