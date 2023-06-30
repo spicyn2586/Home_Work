29d20346454331668b6aacf9568b88c3be3a358e
SpitcynAV <spicyn.2000@mail.ru>
2023-06-30 16:40:21 +0300

    Завершил задание 10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.WriteLine("Введите трехзначное число :");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{number} -> ");
+number = number % 100;
+number = number / 10;
+Console.Write($"{number}");
