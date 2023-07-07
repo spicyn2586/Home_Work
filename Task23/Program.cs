930c90604e483379f14c07bf7405bf6d7b50ec16
SpitcynAV <spicyn.2000@mail.ru>
2023-07-07 18:38:02 +0300

    Выполнил задание Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+5 ->
+1 | 1
+2 | 8
+3 | 27
+4 | 64
+5 | 125 */
+
+Console.WriteLine("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+Console.WriteLine($"{number} ->");
+while (i <= number)
+{
+    Square(i);
+    i++;
+}
+
+int Square (int sqr)
+{
+    double sqrs = Math.Pow(sqr, 3);
+    Console.WriteLine($"{sqr} | {sqrs}");
+    return sqr;
+}
