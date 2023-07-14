acc8ac53f7b9b079eebd04d95bb1b0735654563d
SpitcynAV <spicyn.2000@mail.ru>
2023-07-14 22:04:38 +0300

    Выполнил Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+Console.WriteLine("Укажите количество элементов в массиве: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArrayRndInt(number);
+PrintArray(arr);
+Console.Write($" -> {CountingEvenNumbers(arr)}");
+
+int[] CreateArrayRndInt(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int CountingEvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+}
