748833076e3fe6cc74d25a69699e92f807d49c32
SpitcynAV <spicyn.2000@mail.ru>
2023-07-21 14:02:22 +0300

    Выполнил Task 47

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -4,8 +4,18 @@ m = 3, n = 4.
 0,5 7 -2 -0,2
 1 -3,3 8 -9,9
 8 7,8 -7,1 9 */
-double[,] a = CreateMatrixRnddouble(4,4,-10,10);
-PrintMatrixDouble(a);
+
+Console.WriteLine("Укажите количество строк в дмумерном массиве: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Укажите количество столбцов в дмумерном массиве: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите начальное значение :");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное значение :");
+int max = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = CreateMatrixRnddouble(rows,colums,min,max);
+PrintMatrixDouble(array);
 
 double[,] CreateMatrixRnddouble(int rows, int colums, int min, int max)
 {
@@ -29,7 +39,7 @@ void PrintMatrixDouble(double[,] array, int round = 1)
         for (int j = 0; j < array.GetLength(1); j++)
         {
             double num = Math.Round(array[i,j], round);
-            Console.Write($"{num} ");
+            Console.Write($"{num, 4} ");
         }
         Console.WriteLine();
     }
